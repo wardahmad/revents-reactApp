b922ae532f584c12bb12d021bb1fefbcfda6614b
wardahmad <whitelight43@hotmail.com>
2020-02-20 21:33:33 +0300

    Uncontrolled Forms- EventForm.js

--- src/features/event/EventForm/EventForm.jsx
+++ src/features/event/EventForm/EventForm.jsx
@@ -1,38 +1,46 @@
-import React, { Component } from 'react'
-import { Button, Form, Segment } from 'semantic-ui-react'
+import React, { Component } from "react";
+import { Button, Form, Segment } from "semantic-ui-react";
 
 export default class EventForm extends Component {
-    render() {
-      const {cancelFormOpen} = this.props;
-        return (
-                  <Segment>
-                    <Form>
-                      <Form.Field>
-                        <label>Event Title</label>
-                        <input placeholder="First Name" />
-                      </Form.Field>
-                      <Form.Field>
-                        <label>Event Date</label>
-                        <input type="date" placeholder="Event Date" />
-                      </Form.Field>
-                      <Form.Field>
-                        <label>City</label>
-                        <input placeholder="City event is taking place" />
-                      </Form.Field>
-                      <Form.Field>
-                        <label>Venue</label>
-                        <input placeholder="Enter the Venue of the event" />
-                      </Form.Field>
-                      <Form.Field>
-                        <label>Hosted By</label>
-                        <input placeholder="Enter the name of person hosting" />
-                      </Form.Field>
-                      <Button positive type="submit">
-                        Submit
-                      </Button>
-                      <Button onClick={cancelFormOpen} type="button">Cancel</Button>
-                    </Form>
-                  </Segment>
-        )
-    }
+  
+  handleFormSubmit = (evt) => {
+    evt.preventDefault();
+    console.log(this.refs.title.value)
+  }
+
+  render() {
+    const { cancelFormOpen } = this.props;
+    return (
+      <Segment>
+        <Form onSubmit={this.handleFormSubmit}>
+          <Form.Field>
+            <label>Event Title</label>
+            <input ref='title' placeholder='Event Title' />
+          </Form.Field>
+          <Form.Field>
+            <label>Event Date</label>
+            <input type='date' placeholder='Event Date' />
+          </Form.Field>
+          <Form.Field>
+            <label>City</label>
+            <input placeholder='City event is taking place' />
+          </Form.Field>
+          <Form.Field>
+            <label>Venue</label>
+            <input placeholder='Enter the Venue of the event' />
+          </Form.Field>
+          <Form.Field>
+            <label>Hosted By</label>
+            <input placeholder='Enter the name of person hosting' />
+          </Form.Field>
+          <Button positive type='submit'>
+            Submit
+          </Button>
+          <Button onClick={cancelFormOpen} type='button'>
+            Cancel
+          </Button>
+        </Form>
+      </Segment>
+    );
+  }
 }
