6bf6c588d73f6f94555902b7941cfb314ada9d04
wardahmad <whitelight43@hotmail.com>
2020-02-21 19:07:58 +0300

    CRUD

--- src/features/event/EventForm/EventForm.jsx
+++ src/features/event/EventForm/EventForm.jsx
@@ -10,16 +10,22 @@ export default class EventForm extends Component {
     hostedBy: ""
   };
 
+  componentDidMount() {
+    if (this.props.selectedEvent !== null){
+      this.setState({
+        ...this.props.selectedEvent
+      })
+    }
+  }
+
   handleFormSubmit = evt => {
     evt.preventDefault();
-    this.props.createEvent(this.state);
+    if (this.state.id){
+      this.props.updateEvent(this.state)
+    } else {
+      this.props.createEvent(this.state);
+    }
   };
-
-  // handleInputChange = evt => {
-  //   this.setState({
-  //     [evt.target.name]: evt.target.value
-  //   });
-  // };
 
   handleInputChange = ({ target: { name, value } }) => {
     this.setState({
--- src/features/event/EventList/EventList.jsx
+++ src/features/event/EventList/EventList.jsx
@@ -1,13 +1,20 @@
-import React, { Component, Fragment } from 'react'
-import EventListItem from './EventListItem'
+import React, { Component, Fragment } from "react";
+import EventListItem from "./EventListItem";
 
 export default class EventList extends Component {
-    render() {
-        return (
-            <Fragment>
-                {this.props.events.map(event => (<EventListItem key={event.id} event={event}/>))}
-                
-            </Fragment>
-        )
-    }
+  render() {
+    const { events, selectEvent, deleteEvent } = this.props;
+    return (
+      <Fragment>
+        {events.map(event => (
+          <EventListItem
+            key={event.id}
+            event={event}
+            selectEvent={selectEvent}
+            deleteEvent={deleteEvent}
+          />
+        ))}
+      </Fragment>
+    );
+  }
 }
