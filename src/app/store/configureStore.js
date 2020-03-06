122b9efca303dd59ab587e8ae7710a9ee8ff52ce
wardahmad <whitelight43@hotmail.com>
2020-03-06 14:04:47 +0300

    reducerUtils.js

--- src/app/store/configureStore.js
+++ src/app/store/configureStore.js
@@ -1,9 +1,10 @@
-import { createStore } from "redux"
+import { createStore } from "redux";
+import {devToolsEnhancer} from 'redux-devtools-extension';
 //import testReducer from "../../features/testarea/testReducer";
 import rootReducer from "../reducers/rootReducer";
 
 export const configureStore = () => {
     //const store = createStore(testReducer)
-    const store = createStore(rootReducer)
+    const store = createStore(rootReducer, devToolsEnhancer())
     return store;
 }
