7a455b5ac532228a6ace35521774b89ac49d7960
wardahmad <whitelight43@hotmail.com>
2020-03-10 03:12:46 +0300

    Edit EventDetailedChat/Header/Info/Page/Sidebar+static data

--- src/features/event/EventDetailed/EventDetailedSidebar.jsx
+++ src/features/event/EventDetailed/EventDetailedSidebar.jsx
@@ -1,10 +1,42 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { Item, Label, Segment } from "semantic-ui-react";
 
-const EventDetailedSidebar = () => {
+const EventDetailedSidebar = ({ attendees }) => {
+  const isHost = false;
   return (
-    <div>
-      <h1>Sidebar</h1>
-    </div>
+    <Fragment>
+      <Segment
+        textAlign='center'
+        style={{ border: "none" }}
+        attached='top'
+        secondary
+        inverted
+        color='teal'
+      >
+        {attendees && attendees.length} {attendees && attendees.length === 1 ? 'Person': 'People'} Going
+      </Segment>
+      <Segment attached>
+        <Item.Group divided>
+          {attendees &&
+            attendees.map(attendee => (
+              <Item key={attendee.id} style={{ position: "relative" }}>
+                {isHost &&
+                <Label
+                  style={{ position: "absolute" }}
+                  color='orange'
+                  ribbon='right'
+                >
+                  Host
+                </Label>}
+                <Item.Image size='tiny' src={attendee.photoURL} />
+                <Item.Content verticalAlign='middle'>
+                  <Item.Header as='h3'>{attendee.name}</Item.Header>
+                </Item.Content>
+              </Item>
+            ))}
+        </Item.Group>
+      </Segment>
+    </Fragment>
   );
 };
 
